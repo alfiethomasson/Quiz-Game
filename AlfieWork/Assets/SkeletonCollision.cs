c2660a5d87299a2624aeec849882c572a77ba718
Alfie Thomasson <alfiethomasson0@gmail.com>
2018-10-15 13:54:52 +0100

    Game work 1510

---
+++ AlfieWork/Assets/SkeletonCollision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SkeletonCollision : MonoBehaviour {
+
+    Animator animator;
+    
+    void Awake()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+         if (other.gameObject.tag == "Player")
+        {
+        
+            animator.SetBool("hit", true);
+        }
+    }
+}

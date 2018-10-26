f74802f1983bea9a46d532a8faed7f957948635f
Alfie Thomasson <alfiethomasson0@gmail.com>
2018-10-26 14:22:00 +0100

    26th 10 take 2

---
+++ AlfieWork/Assets/SkeletonCollision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SkeletonCollision : MonoBehaviour {
+
+    float delay = 10.0f;
+    public Rigidbody rb;
+    Animator animator;
+    
+    void Awake()
+    {
+        animator = gameObject.GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+
+         if (other.gameObject.tag == "Sword")
+        {
+        
+            animator.SetBool("hit", true);
+            rb.isKinematic = true;
+            rb.detectCollisions = false;
+            Destroy(gameObject, delay);
+        }
+    }
+
+    /*void DisableRagdoll()
+    {
+
+        rb.isKinematic = true;
+        rb.detectCollisions = false;
+
+    }
+    */
+   
+}
---
+++ AlfieWork/Assets/SwordSwing.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordSwing : MonoBehaviour {
+
+    Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = gameObject.GetComponent<Animator>();
+
+
+    }
+	
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            anim.SetBool("Swing", true);
+        }
+
+
+    }
+
+	
+
+}
